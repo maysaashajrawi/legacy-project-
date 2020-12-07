03f04782781d7535a83a6aea0da64e62c0fb6544
saraalrajabii <aloqailysura@gmail.com>
2020-12-07 02:17:04 +0200

    profile-page-clientside-adding-info&image

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'mdbreact/dist/css/mdb.css';
 import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 
-
+import Profile from './components/Profile';
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import AddItems from './components/AddItems';
@@ -25,11 +25,12 @@ function App() {
 
         <Route path = "/homepage" component = { Homepage } />
         <Route path="/ItemsList" component={ItemsList} />
-        <ProtectedRoute path="/addItems" component={AddItems} isAuth={localStorage.length>0}/>
+        <Route path="/addItems" component={AddItems} />
         <Route path = "/addUser"  component = { Signup } />
         <Route path = "/login" component = { Login } />
         <Route path = "/edit/:id" component = { EditItems }/>
         <Route path = "/logout" component = { Login } />
+        <Route path="/profile" component={Profile}/>
     
       </div>
     </Router>
--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import { Link ,withRouter } from "react-router-dom" ;
+import { withRouter } from "react-router-dom" ;
 import Footer from './Footer';
 
 
