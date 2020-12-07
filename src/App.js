ff8dcbf747b4d095d0c293f85780df7200eb1284
Taghreed81 <tagrid21003@hotmail.com>
2020-12-07 19:58:02 +0200

    navbarFromLogin

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import ProtectedRoute from './components/ProtectedRoute';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+import "./App.css";
 
+import Homepage from "./components/Homepage";
 
-import Homepage from './components/Homepage';
-import Navbar from './components/Navbar';
-import AddItems from './components/AddItems';
-import ItemsList from './components/ItemsList';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import EditItems from './components/edit';
-
+import AddItems from "./components/AddItems";
+import ItemsList from "./components/ItemsList";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import EditItems from "./components/edit";
+import Homepage2 from "./components/Homepage2.js";
 
 function App() {
-  
   return (
-    <Router className = "container">
+    <Router className="container">
       <div>
-        
-        <Navbar />
-
-        <Route path = "/homepage" component = { Homepage } />
-        <ProtectedRoute path="/ItemsList" component={ItemsList} isAuth={localStorage.length>0}/>
-        <ProtectedRoute path="/addItems" component={AddItems} isAuth={localStorage.length>0}/>
-        <Route path = "/addUser"  component = { Signup } />
-        <Route path = "/login" component = { Login } />
-        <Route path = "/edit/:id" component = { EditItems }/>
-        <Route path = "/logout" component = { Login } />
-    
+        <Route path="/homepage2" component={Homepage2} />
+        <Route path="/homepage" component={Homepage} />
+        <Route path="/ItemsList" component={ItemsList} />
+        <Route path="/addItems" component={AddItems} />
+        <Route path="/addUser" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/edit/:id" component={EditItems} />
+        <Route path="/logout" component={Login} />
       </div>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,48 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-
   render() {
     return (
       <div>
-      <nav style = {{ color: "red" }} className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/Homepage" className="navbar-brand">DAFAH</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto" >
-          <li className="navbar-item" >
-          <Link to="/AddItems" className="nav-link">Add Items</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/ItemsList" className="nav-link">Clothes</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/login" className="nav-link">Login</Link>
-          </li>
-          <li className="navbar-item"onClick={logout}>
-          <Link to="/logout" className="nav-link">Log out</Link>
-          </li>
-        </ul>
-        </div>
-      </nav>
+        <nav
+          style={{ color: "red" }}
+          className="navbar navbar-dark bg-dark navbar-expand-lg"
+        >
+          <Link to="/Homepage" className="navbar-brand">
+            DAFAH
+          </Link>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              {/* <li className="navbar-item">
+                <Link to="/AddItems" className="nav-link">
+                  Add Items
+                </Link>
+              </li> */}
+              <li className="navbar-item">
+                <Link to="/ItemsList" className="nav-link">
+                  Clothes
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              {/* <li className="navbar-item" onClick={logout}>
+                <Link to="/logout" className="nav-link">
+                  Log out
+                </Link>
+              </li> */}
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
 }
 
-
- function logout(){
-
+function logout() {
   window.localStorage.clear();
-  window.location = '/Homepage'
-
- }
+  window.location = "/Homepage";
+}
