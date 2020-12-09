89a8c853cc25e5068b279727bf738343a3f21b8f
saraalrajabii <saraalrajabii12@gmail.com>
2020-12-09 17:24:13 +0200

    image-saving-done

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,8 @@
 const mongoose= require('mongoose');
 const { schema } = require('./addItems.model');
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
 
 const Schema = mongoose.Schema;
 //creat the Schema, what data we want to be saved
@@ -15,25 +18,38 @@ const userSchema = new Schema({
     password : {
         type: String,
         minlength: 5,
-        required: true
+        required: true,
+        trim: true
         
     },
     phone: {
         
         type: Number,
          unique:true,
-        min:10
+        min:10,
+        trim: true,
+
     },
     address:{
         type: String,
         minlength: 4,
-        required: true
+        required: true,
+        trim: true
+    },
+    image:
+    {
+        type: String,
+        required: true,
+        trim: true
     }
   
-   
-    
 });
 
+
+
+
 const User = mongoose.model('User', userSchema);
+
+// module.exports = new mongoose.model('Image', userSchema);
 
 module.exports = User;
